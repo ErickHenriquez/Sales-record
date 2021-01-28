241476fc1d856bcb4d839e11759ae52dce4b383d
= <=>
2021-01-27 21:29:44 -0300

    Backend y Frontend Inicializados

---
+++ BACKEND/SalesRecord/src/main/java/cl/erick/avla/SalesRecordApplication.java
@@ -0,0 +1,13 @@
+package cl.erick.avla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesRecordApplication.class, args);
+	}
+
+}
---
+++ BACKEND/SalesRecord/src/test/java/cl/erick/avla/SalesRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.erick.avla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
